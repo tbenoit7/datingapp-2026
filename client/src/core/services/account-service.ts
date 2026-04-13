58d75bb47f7f126989745d1dc9e61131cd54f921
Tamatoa <tamatoabenoit@gmail.com>
2026-04-13 13:47:50 -1000

    End of section 9 - improving UI

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -2,15 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {    
-  private http = inject(HttpClient);
-  currentUser  = signal<User | null>(null);
-
-  baseUrl = 'https://localhost:5001/api/';
+  private http    = inject(HttpClient);
+  currentUser     = signal<User | null>(null);
+  private baseUrl = environment.apiUrl;
 
   register(creds: RegisterCreds){
     return this.http.post<User>(this.baseUrl + 'account/register', creds).pipe(
