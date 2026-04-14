84f1f50274c32a62376904ea22b26e6b98d8f84b
Tamatoa <tamatoabenoit@gmail.com>
2026-04-13 17:01:03 -1000

    Milieu section 10

--- client/src/features/members/member-profile/member-profile.ts
+++ client/src/features/members/member-profile/member-profile.ts
@@ -1,21 +1,54 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Member } from '../../../types/member';
+import { EditableMember, Member } from '../../../types/member';
 import { DatePipe } from '@angular/common';
+import { MemberService } from '../../../core/services/member-service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastService } from '../../../core/services/toast-service';
 
 @Component({
   selector: 'app-member-profile',
-  imports: [DatePipe],
+  imports: [DatePipe, FormsModule],
   templateUrl: './member-profile.html',
   styleUrl: './member-profile.css',
 })
-export class MemberProfile implements OnInit {
-  private route = inject(ActivatedRoute);
-  protected member = signal<Member | undefined>(undefined);
+export class MemberProfile implements OnInit, OnDestroy {
+  @ViewChild('editForm') editForm?: NgForm
+  private route           = inject(ActivatedRoute);
+  private toast           = inject(ToastService);
+  protected memberService = inject(MemberService);
+  protected member        = signal<Member | undefined>(undefined);
+  protected editableMember: EditableMember = {
+    displayName: '',
+    description: '',
+    city: '',
+    country: ''
+  }
+
+  updateProfile() {
+    if (this.member()) return;
+    const updatedMember= {...this.member(), ...this.editableMember}
+    console.log(updatedMember);
+    this.toast.success('Profile updated successfully');
+    this.memberService.editMode.set(false);
+  }
 
   ngOnInit(): void {
     this.route.parent?.data.subscribe(data => {
       this.member.set(data['member']);
     })
+
+    this.editableMember = {
+      displayName: this.member()?.displayName || '',
+      description: this.member()?.description || '',
+      city: this.member()?.city || '',
+      country: this.member()?.country || '',
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.memberService.editMode()) {
+      this.memberService.editMode.set(false);
+    }
   }
 }
